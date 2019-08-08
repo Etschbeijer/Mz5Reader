da2d7864f780da3b5cb1decd1347271cdf173b1d
patrick blume <PatrickBlume@gmx.de>
2019-08-08 20:32:02 +0200

    Improve function to initialize object in unmanaged memory

--- Mz5Reader/Script.fsx
+++ Mz5Reader/Script.fsx
@@ -58,8 +58,8 @@ let readStringArray (fileId:int64) (datasetPath:string) (recordLength:int) =
             let content = System.Text.Encoding.ASCII.GetString(slice).Trim()
             dataset <- content::dataset
 
-        if not (typeId = 0L) then H5T.close(typeId)              |> ignore
-        if not (dataSpaceId = 0L) then H5S.close(dataSpaceId)    |> ignore
+        if not (typeId = 0L) then H5T.close(typeId)             |> ignore
+        if not (dataSpaceId = 0L) then H5S.close(dataSpaceId)   |> ignore
         if not (dataSetId = 0L) then H5D.close(dataSetId)       |> ignore
 
         let datasetOut = dataset |> Array.ofList
@@ -81,66 +81,19 @@ let iterate(objectId:int64) (namePtr:IntPtr) (info:byref<H5O.info_t>) (op_data:I
             1
         else
             0
-    //new IntPtr()
-
 
 type Delegate1 = delegate of int64*nativeint*byref<H5O.info_t>*nativeint -> int
 
 let invokeDelegate (dlg:Delegate1) (objectId:int64) (namePtr:IntPtr) (info:byref<H5O.info_t>) (op_data:IntPtr) =
     dlg.Invoke(objectId, namePtr, &info, op_data)
 
-let directory = __SOURCE_DIRECTORY__
 
+
+
+
+let directory = __SOURCE_DIRECTORY__
 let stormPath       = directory + "/HDF5Files/storm.h5"
 let experimentPath  = directory + "/HDF5Files/Experiment_1.mz5"
-
-//H5.``open``()
-//H5F.is_hdf5(stormPath)
-//let fileId = H5F.``open``(stormPath, H5F.ACC_RDONLY)
-//let rootId = H5G.``open``(fileId, "/")
-
-//let del1 = new Delegate1(fun objectId namePtr info op_data -> (iterate objectId namePtr (&info) op_data))   
-   
-//let getDataSetNames(fileId) =
-//    H5O.visit(fileId, H5.index_t.NAME, H5.iter_order_t.INC, new H5O.iterate_t((fun objectId namePtr info op_data -> invokeDelegate del1 objectId namePtr (&info) op_data)), new IntPtr())
-     
-//H5G.close(rootId)
-
-////// Print out the information that we found
-//for i in datasetNames do
-//    printfn "%s" i
-
-//for i in groupNames do
-//    printfn "%s" i
-
-//getDataSetNames()
-
-let getMz5ID (path:string) =
-    H5.``open``()       |> ignore
-    if H5F.is_hdf5(path) = 1 then
-        H5F.``open``(path, H5F.ACC_RDONLY)
-    else
-        failwith "File is no HDF5 file"
-
-let getDataSetID (fileID:int64) (dateSet:string) =
-    H5D.``open``(fileID, dateSet)
-
-let checkForObjectType (dataSetID:int64) (dateSet:string) =
-    let mutable gInfo = new H5O.info_t()
-    H5O.get_info_by_name(dataSetID, dateSet, &gInfo) |> ignore
-    gInfo.``type``
-
-let mz5ID = getMz5ID experimentPath
-
-//let cvParamID = getDataSetID mz5ID "/CVParam"
- 
-//let cvParamType = checkForObjectType cvParamID "/CVParam"
-
-let fileID = getMz5ID stormPath
-
-let stormID = getDataSetID fileID "/Data/Storm"
- 
-let dataSetType = checkForObjectType stormID "/Data/Storm"
 
 let byteToSingleArray (byteArray: byte[]) = 
     let singles = Array.zeroCreate<Int32> (byteArray.Length/4)
@@ -157,6 +110,7 @@ let windowedArray  (windowSize:int32) (vector:'a[]) =
         else
             List.rev acc
             |> Array.ofList
+            |> array2D
     loop List.empty 0
 
 let getAllDataOfDataSet (fileID:int64) (dsName:string) (bitConverter:byte[]->'T) =
@@ -166,8 +120,9 @@ let getAllDataOfDataSet (fileID:int64) (dsName:string) (bitConverter:byte[]->'T)
     let rank                = H5S.get_simple_extent_ndims(spaceID)
     let dims                = Array.zeroCreate<UInt64>(rank)
     let maxDims             = Array.zeroCreate<UInt64>(rank)
-    let spaceID             = H5D.get_space(dataSetID)
-    H5S.get_simple_extent_dims(spaceID, dims, maxDims) |> ignore
+
+    let nDims = H5S.get_simple_extent_dims(spaceID, dims, maxDims)
+
     let sizeData            = H5T.get_size(typeID)
     let size                = sizeData.ToInt32()
     let byteArrayElements   =
@@ -175,54 +130,220 @@ let getAllDataOfDataSet (fileID:int64) (dsName:string) (bitConverter:byte[]->'T)
         |> Array.fold (fun length item -> length * item) 1uL
     let dataBytes   = Array.zeroCreate<Byte> (Convert.ToInt32(byteArrayElements) * size)
     let pinnedArray = GCHandle.Alloc(dataBytes, GCHandleType.Pinned)
-    H5D.read(dataSetID, typeID, int64 H5S.ALL, int64 H5S.ALL, H5P.DEFAULT, pinnedArray.AddrOfPinnedObject()) |> ignore
+
+    let readerStatus = H5D.read(dataSetID, typeID, int64 H5S.ALL, int64 H5S.ALL, H5P.DEFAULT, pinnedArray.AddrOfPinnedObject())
+    printfn "readerStatus %A" readerStatus
     pinnedArray.Free()
     bitConverter dataBytes
-    
-let test = (getAllDataOfDataSet stormID "/Data/Storm" byteToSingleArray)
-windowedArray 57 test
 
-let getSpecificDataOfDataSet (fileID:int64) (dsName:string) (bitConverter:byte[]->'T) =
+let getSpecificDataOfDataSet (path:string) (dsName:string) (start:UInt64[]) (count:UInt64[]) (stride:UInt64[]) (block:UInt64[]) (bitConverter:byte[]->'T) =
 
-    let dataSetID           = H5D.``open``(fileID, dsName, H5P.DEFAULT)
-    let spaceID             = H5D.get_space(dataSetID)
-    let typeID              = H5D.get_type(dataSetID)
-    let rank                = H5S.get_simple_extent_ndims(spaceID)
-    let dims                = Array.zeroCreate<UInt64>(rank)
-    let maxDims             = Array.zeroCreate<UInt64>(rank)
-    let spaceID             = H5D.get_space(dataSetID)
+    let fileID      =
+        H5.``open``()   |> ignore
+        if H5F.is_hdf5(path) = 1 then
+            H5F.``open``(path, H5F.ACC_RDONLY)
+        else
+            failwith "File is no HDF5 file"
+    let dataSetID   = H5D.``open``(fileID, dsName, H5P.DEFAULT)
+    let spaceID     = H5D.get_space(dataSetID)
+    let typeID      = H5D.get_type(dataSetID)
+    let rank        = H5S.get_simple_extent_ndims(spaceID)
+    let sizeData    = H5T.get_size(typeID)
+    let size        = sizeData.ToInt32()
+    let dims        = [|Convert.ToUInt64(3); Convert.ToUInt64(4)|]
+    let memoryID    = H5S.create_simple(rank, dims, null) 
 
-    H5S.get_simple_extent_dims(spaceID, dims, maxDims) |> ignore
+    let status      = H5S.select_hyperslab(spaceID, H5S.seloper_t.SET, start, stride, count, block)
 
-    let sizeData            = H5T.get_size(typeID)
-    //let size                = sizeData.ToInt32()
-    //let byteArrayElements   = 
-    //    //Convert.ToUInt64(dims.[0])
-    //    dims
-    //    |> Array.fold (fun length item -> length * item) 1uL
-    //let dataBytes   = Array.zeroCreate<Byte> (Convert.ToInt32(byteArrayElements) * size)
-    let rData = Array.zeroCreate<Byte> 48
-    let pinnedArray = GCHandle.Alloc(rData, GCHandleType.Pinned)
+    let byteArrayElements  =
+        dims
+        |> Array.fold (fun length item -> length * item) 1uL
+    let dataBytes   = Array.zeroCreate<Byte> (Convert.ToInt32(byteArrayElements) * size)
+    let pinnedArray = GCHandle.Alloc(dataBytes, GCHandleType.Pinned)
 
-    let start   = [|Convert.ToUInt64(1); Convert.ToUInt64(2)|]
-    let count   = [|Convert.ToUInt64(3); Convert.ToUInt64(4)|]
-    let stride  = [|Convert.ToUInt64(1); Convert.ToUInt64(1)|]
-    let block   = [|Convert.ToUInt64(1); Convert.ToUInt64(1)|]    
+    let readerStatus    = H5D.read(dataSetID, typeID, memoryID, spaceID, H5P.DEFAULT, pinnedArray.AddrOfPinnedObject())
 
-    let memoryID = H5S.create_simple(rank, dims, null)
-    let status = H5S.select_hyperslab(spaceID, H5S.seloper_t.SET, start, stride, count, block)
-
-    H5D.write(dataSetID, typeID, memoryID, spaceID, H5P.DEFAULT, pinnedArray.AddrOfPinnedObject()) |> ignore    
-    H5D.read(dataSetID, typeID, int64 H5S.ALL, int64 H5S.ALL, H5P.DEFAULT, pinnedArray.AddrOfPinnedObject()) |> ignore   
-    
     pinnedArray.Free()
-    bitConverter rData
-    
-    
-    
+    bitConverter dataBytes
 
-//(getSpecificDataOfDataSet stormID "/Data/Storm" byteToSingleArray)
-
-let testII = (getSpecificDataOfDataSet stormID "/Data/Storm" byteToSingleArray)
+let start       = [|Convert.ToUInt64(1); Convert.ToUInt64(2)|]
+let count       = [|Convert.ToUInt64(3); Convert.ToUInt64(4)|]
+let stride      = [|Convert.ToUInt64(1); Convert.ToUInt64(1)|]
+let block       = [|Convert.ToUInt64(1); Convert.ToUInt64(1)|]
+    
+let testII = getSpecificDataOfDataSet stormPath "/Data/Storm" start count stride block byteToSingleArray
 
 testII
+|> windowedArray 4
+
+
+type CvParam =
+    {
+        Value   : string
+        CvRefID : UInt32
+        URefID  : UInt32
+    }
+
+let createCvParam value cvRefID uRefID =
+    {
+        CvParam.Value   = value  
+        CvParam.CvRefID = cvRefID
+        CvParam.URefID  = uRefID 
+    }
+
+[<StructLayout(LayoutKind.Explicit)>]
+type CVParam =
+    struct
+        [<FieldOffset(0)>]
+        val mutable Value   : string
+        [<FieldOffset(127)>]
+        val mutable CvRefID : UInt32
+        [<FieldOffset(131)>]
+        val mutable URefID  : UInt32
+    end
+ 
+
+//type SpectrumMetaData =
+//    {
+//        id
+//        spotID
+//        cvStart
+//        cvEnd
+//        uStart
+//        uEnd
+//        refStart
+//        refEnd
+//        scanListCvStart
+//        scanListCvEnd
+//        scanListUStart
+//        scanListUEnd
+//        scanListRefStart
+//        scanListRefEnd
+//        scanList
+//        precursors
+//        products
+//        refDataProcessing
+//        refSourceFile
+//    }
+
+let buildCvParam (bytes:Byte[]) (size:int) =
+    let cvParams = Array.zeroCreate<CvParam> (bytes.Length/size)
+    let rec loop acc n =
+        if Array.isEmpty acc then
+            cvParams
+        else
+            let value = 
+                Array.take 128 acc
+                |> byteToStringArray
+            let cvRefID =
+                Array.skip 128 acc
+                |> Array.take 4
+                |> byteToSingleArray
+                |> Array.head
+                |> Convert.ToUInt32
+            let uRefID =
+                Array.skip 132 acc
+                |> Array.take 4 
+                |> byteToSingleArray
+                |> Array.head
+                |> Convert.ToUInt32
+            let cvParam = createCvParam value cvRefID uRefID
+            cvParams.[n] <- cvParam
+            loop (Array.skip size acc) (n+1)
+    loop bytes 0
+
+#nowarn "9"
+
+let getDataOfDataSet<'T> (path:string) (dsName:string) =
+
+    let fileID      =
+        H5.``open``()   |> ignore
+        if H5F.is_hdf5(path) = 1 then
+            H5F.``open``(path, H5F.ACC_RDONLY)
+        else
+            failwith "File is no HDF5 file"
+    let dataSetID   = H5D.``open``(fileID, dsName, H5P.DEFAULT)
+    let spaceID     = H5D.get_space(dataSetID)
+    //let typeID      = H5D.get_type(dataSetID)
+    let rank        = H5S.get_simple_extent_ndims(spaceID)
+    let dims        = Array.zeroCreate<UInt64>(rank)
+    let maxDims     = Array.zeroCreate<UInt64>(rank)    
+
+    let nDims = H5S.get_simple_extent_dims(spaceID, dims, maxDims)
+
+    //let sizeData            = H5T.get_size(typeID)
+    //let size                = sizeData.ToInt32()
+    let byteArrayElements   =
+        dims
+        |> Array.fold (fun length item -> length * item) 1uL
+    let dataBytes       = Array.zeroCreate<'T> ((int byteArrayElements) * (sizeof<'T>))
+
+    let nativeID        = System.Runtime.InteropServices.Marshal.UnsafeAddrOfPinnedArrayElement(dataBytes,0)
+    let intptr          = new System.IntPtr(nativeID.ToPointer())
+    //let pinnedArray     = GCHandle.Alloc(dataBytes, GCHandleType.Pinned)
+
+    let compoundTypeID  = H5T.create(H5T.class_t.COMPOUND, nativeint(sizeof<'T>))
+    //let changeA pointer newA =
+    //    let mutable (structure:CVParam) = FSharp.NativeInterop.NativePtr.read pointer
+    //    structure.Value <- newA
+    //    FSharp.NativeInterop.NativePtr.write pointer structure
+    let insertValue     = H5T.insert(compoundTypeID, "value"    , 0n    , H5T.C_S1)
+    let insertValue     = H5T.insert(compoundTypeID, "cvrefID"  , 127n  , H5T.NATIVE_UINT32)
+    let insertValue     = H5T.insert(compoundTypeID, "urefID"   , 0n    , H5T.NATIVE_UINT32)
+
+    let readStatus  = H5D.read(dataSetID, compoundTypeID, int64 H5S.ALL, int64 H5S.ALL, H5P.DEFAULT, nativeID)
+    printfn "%A readStatus" readStatus
+    
+    //let cvParams = Array.zeroCreate<CvParam> (dataBytes.Length/size)
+    
+    let reclaimStatus   = H5D.vlen_reclaim(compoundTypeID, spaceID, H5P.DEFAULT, nativeID)
+    printfn "%A reclaimStatus" reclaimStatus
+    //pinnedArray.Free()
+    dataBytes
+    
+
+let getSpecificData (path:string) (dsName:string) (start:int) (amount:int) (bitConverter:byte[]->int->'T) =
+
+    let start   = [|Convert.ToUInt64(start)|]
+    let count   = [|Convert.ToUInt64(amount)|]
+    let stride  = [|Convert.ToUInt64(1)|]
+    let block   = [|Convert.ToUInt64(1)|]
+
+    let fileID      =
+        H5.``open``()   |> ignore
+        if H5F.is_hdf5(path) = 1 then
+            H5F.``open``(path, H5F.ACC_RDONLY)
+        else
+            failwith "File is no HDF5 file"
+    let dataSetID   = H5D.``open``(fileID, dsName, H5P.DEFAULT)
+    let spaceID     = H5D.get_space(dataSetID)
+    let typeID      = H5D.get_type(dataSetID)
+    let rank        = H5S.get_simple_extent_ndims(spaceID)
+    let dims        = Array.copy count   
+
+    let sizeData            = H5T.get_size(typeID)
+    let size                = sizeData.ToInt32()
+    let byteArrayElements   =
+        dims
+        |> Array.fold (fun length item -> length * item) 1uL
+    let dataBytes       = Array.zeroCreate<Byte> (Convert.ToInt32(byteArrayElements) * size)
+
+    let memoryID        = H5S.create_simple(rank, dims, null)
+
+    let hyperSlabStatus = H5S.select_hyperslab(spaceID, H5S.seloper_t.SET, start, stride, count, block) 
+    
+    let pinnedArray     = GCHandle.Alloc(dataBytes, GCHandleType.Pinned)
+
+    let readerStatus    = H5D.read(dataSetID, typeID, memoryID, spaceID, H5P.DEFAULT, pinnedArray.AddrOfPinnedObject())
+
+    pinnedArray.Free()
+    bitConverter dataBytes size
+
+
+    
+//(getSpecificData experimentPath "/CVParam" 0 5 buildCvParam)
+
+
+(getDataOfDataSet<CvParam> experimentPath "/CVParam")
+
+
